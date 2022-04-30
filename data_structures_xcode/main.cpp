dc52cda92cc7f32cbd92083f80e86bc36a781646
jasonketterer <jasonrketterer@gmail.com>
2022-04-30 12:48:15 -0400

    Initial Commit

---
+++ data_structures_xcode/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  data_structures_xcode
+//
+//  Created by Jason Ketterer on 4/30/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
